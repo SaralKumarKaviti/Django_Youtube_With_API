c7779b42d973230494030c1506fb193e437adac3
SaralKumarKaviti <saralkumar238@gmail.com>
2019-10-01 15:45:15 +0530

    statistics

--- django_youtube/core/urls.py
+++ django_youtube/core/urls.py
@@ -4,12 +4,13 @@ from django.urls import path
 
 from .views import HomePageView,AuthorizeView,OAuth2CallbackView
 
+from core import views
 
 urlpatterns=[
 	path('',HomePageView.as_view(), name='home'),
 	path('authorize/',AuthorizeView.as_view(), name='authorize'),
 	path('oauth2callback/',OAuth2CallbackView.as_view(), name='oauth2callback'),
-	
+	path('youtube_search/',views.youtube_search)
 ]
 
 if settings.DEBUG:
--- django_youtube/core/views.py
+++ django_youtube/core/views.py
@@ -8,17 +8,94 @@ from django.conf import settings
 
 from oauth2client.client import flow_from_clientsecrets, OAuth2WebServerFlow
 from oauth2client.contrib import xsrfutil
+from oauth2client.tools import argparser
 from oauth2client.contrib.django_util.storage import DjangoORMStorage
 from .models import CredentialsModel
 
 import tempfile
 from django.http import HttpResponse,HttpResponseBadRequest
-from googleapiclient.discovery import build
+import googleapiclient.discovery
 from googleapiclient.http import MediaFileUpload
 from embed_video.fields import EmbedVideoField
-
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from django.views.decorators.csrf import requires_csrf_token
 
 # Create your views here.
+DEVELOPER_KEY="AIzaSyBC47N-lpeWWnXEG4yMHmpcVC6b3rQwXEY"
+YOUTUBE_API_SERVICE_NAME="youtube"
+YOUTUBE_API_VERSION="v3"
+
+
+@requires_csrf_token
+def youtube_search(request):
+	q="My Python Youtube Video" 
+	max_results=50
+	order="relevance"
+	token=None
+	location=None
+	location_radius=None
+	print(q)
+	youtube=googleapiclient.discovery.build(YOUTUBE_API_SERVICE_NAME,YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY)
+	search_response=youtube.search().list(q=q,type="video",pageToken=token,order=order,part="snippet",maxResults=max_results,location=location,locationRadius=location_radius).execute()
+
+
+	title=[]
+	channelId=[]
+	channelTitle=[]
+	categoryId=[]
+	videoId=[]
+	viewCount=[]
+	likeCount=[]
+	dislikeCount=[]
+	commentCount=[]
+	favoriteCount=[]
+	category=[]
+	tags=[]
+	videos=[]
+
+	for search_result in search_response.get("items",[]):
+		if search_result['id']['kind']=='youtube#video':
+			title.append(search_result['snippet']['title'])
+			videoId.append(search_result['id']['videoId'])
+
+		response=youtube.videos().list(
+			part='statistics,snippet',id=search_result['id']['videoId']).execute()
+
+		channelId.append(response['items'][0]['snippet']['channelId'])
+		channelTitle.append(response['items'][0]['snippet']['channelTitle'])
+		categoryId.append(response['items'][0]['snippet']['categoryId'])
+		favoriteCount.append(response['items'][0]['statistics']['favoriteCount'])
+		viewCount.append(response['items'][0]['statistics']['viewCount'])
+		likeCount.append(response['items'][0]['statistics']['likeCount'])
+		dislikeCount.append(response['items'][0]['statistics']['dislikeCount'])
+
+		if 'commentCount' in response['items'][0]['statistics'].keys():
+			commentCount.append(response['items'][0]['statistics']['commentCount'])
+		else:
+			commentCount.append([])
+
+		if 'tags' in response['items'][0]['snippet'].keys():
+			tags.append(response['items'][0]['snippet']['tags'])
+		# else:
+		# 	tags.append()
+		youtube_dict={
+		'tags':tags,
+		'channelId':"63lFwLa5B1Q",
+		'channelTitle':channelTitle,
+		'categoryId':categoryId,
+		'title':title,
+		'videoId':videoId,
+		'viewCount':viewCount,
+		'likeCount':likeCount,
+		'dislikeCount':dislikeCount,
+		'commentCount':commentCount,
+		'favoriteCount':favoriteCount
+
+		}
+		print(youtube_dict)
+	return HttpResponse("youtube_dict")
+	#return render(request,'core/home.html')
 
 class YouTubeForm(forms.Form):
 	video = forms.FileField()
@@ -26,7 +103,7 @@ class YouTubeForm(forms.Form):
 class HomePageView(FormView):
 	template_name='core/home.html'
 	form_class=YouTubeForm
-
+	
 	def form_valid(self,form):
 		fname=form.cleaned_data['video'].temporary_file_path()
 		storage=DjangoORMStorage(
@@ -37,8 +114,8 @@ class HomePageView(FormView):
 
 		body={
 			'snippet':{
-				'title':'My Django Youtube Video',
-				'description':'My Django Youtube Description',
+				'title':'My Python Youtube Video',
+				'description':'My Python Youtube Description',
 				'tags':'django,howto,video,api',
 				'categoryId':'27',
 
@@ -48,6 +125,8 @@ class HomePageView(FormView):
 				'privacyStatus':'unlisted'
 			}
 		}
+	
+	
 
 		with tempfile.NamedTemporaryFile('wb',suffix='yt-django') as tmpfile:
 			with open(fname,'rb') as fileobj:
@@ -60,6 +139,14 @@ class HomePageView(FormView):
 				result=insert_request.execute()
 				video_id=result['id']
 				html='<html><body><iframe width="420" height="315" src="https://www.youtube.com/embed/{0}"></iframe></body></html>'.format(video_id)
+				#print(api_key)
+		
+		#print(type(youtube))
+	
+
+
+		
+
 		return HttpResponse(html)
 
 
@@ -103,9 +190,3 @@ class OAuth2CallbackView(View):
 		return redirect('/')
 
 
-'''class Item(View):
-	def get(self,request,*args,**kwargs):
-		videos=Item.object.all()
-		context={'videos',videos}
-		return render('show_video.html',context)
-'''
